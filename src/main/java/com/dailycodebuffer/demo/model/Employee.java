747d755a82ca562ffe7d36aebd1e8f483631da66
maubmz <mauriciobaeza488@gmail.com>
2023-12-13 13:49:53 -0800

    Spring Proyecto Demo

---
+++ src/main/java/com/dailycodebuffer/demo/model/Employee.java
@@ -0,0 +1,22 @@
+package com.dailycodebuffer.demo.model;
+
+public class Employee {
+    private int empId;
+    private String empName;
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empdId) {
+        this.empId = empdId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+}
