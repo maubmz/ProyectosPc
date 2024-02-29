2e0bcc7e491049cad1de618868de40df9ea448ce
maubmz <mauriciobaeza488@gmail.com>
2024-02-28 20:02:38 -0600

    Seccion 3 Curso Udemy
    video 101

---
+++ expense_tracker/lib/expenses.dart
@@ -0,0 +1,41 @@
+import 'package:expense_tracker/expense.dart';
+
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _Expenses();
+  }
+}
+
+class _Expenses extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Text('The chart'),
+          Text('Expenses list...'),
+        ],
+      ),
+    );
+  }
+}
